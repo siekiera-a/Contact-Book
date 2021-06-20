ba5a05e5e7370a46aaac61b0c422371b35a26631
siekiera-a <siekiera.a31@gmail.com>
2021-06-20 15:13:02 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM postgres:latest
+
+ENV POSTGRES_USER=postgres
+ENV POSTGRES_PASSWORD=password123!
+ENV POSTGRES_DB=contact_book
---
+++ src/main/java/pl/siekiera/contactbook/ContactBookApplication.java
@@ -0,0 +1,13 @@
+package pl.siekiera.contactbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookApplication.class, args);
+	}
+
+}
